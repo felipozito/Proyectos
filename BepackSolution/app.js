c5d7204c23e7e8f808dd30bc1faef898c5d7750a
felipozito <felipozito@hotmail.com>
2022-05-01 17:58:23 -0500

    Slider completo y Nosotros en la reconstruccion BEPACK FALTA SERVICIOS PRODUCTOS Y NOSOTROS

--- BepackSolution/app.js
+++ BepackSolution/app.js
@@ -1,6 +1,8 @@
 const btn = document.querySelector("label");
 const menu = document.querySelector(".list");
 const item = document.querySelectorAll(".item");
+const slides = document.getElementsByClassName("mySlides");
+const dots = document.getElementsByClassName("dot");
 
 btn.addEventListener("click", () => {
       menu.classList.toggle("show");
@@ -9,3 +11,22 @@ btn.addEventListener("click", () => {
 item.forEach((element) => {
       element.addEventListener("click", () => menu.classList.toggle("show"));
 });
+
+let slideIndex = 0;
+SlideShow();
+
+function SlideShow() {
+      for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {
+            slideIndex = 1;
+      }
+      for (let i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex - 1].style.display = "block";
+      dots[slideIndex - 1].className += " active";
+      setTimeout(SlideShow, 3000); // Change image every 2 seconds
+}
