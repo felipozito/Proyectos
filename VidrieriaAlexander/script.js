287fdc6d62b546551d6ec7cf5cd886c9aafcf1f5
felipozito <felipozito@hotmail.com>
2022-06-27 17:42:05 -0500

    Navbar Responsive v2

--- VidrieriaAlexander/script.js
+++ VidrieriaAlexander/script.js
@@ -1,12 +1,21 @@
 const navbar = document.querySelector(".navbar");
 const navbarItem = document.querySelector(".navbar_item");
 const navResponsive = document.querySelector(".ico");
-
 window.addEventListener("scroll", () => {
       navbar.classList.toggle("sticky", window.scrollY > 0);
 });
 navResponsive.addEventListener("click", (e) => {
       navbarItem.classList.toggle("show");
+      navbarItem.classList.forEach((item) => {
+            if (item == "show") {
+                  navResponsive.textContent = "x";
+            } else {
+                  navResponsive.textContent = "=";
+            }
+      });
+      //   if (navbarItem.classList == "show") {
+      //         navResponsive.setContent = "X";
+      //   }
 });
 
 window.onscroll = () => {
